00a89314602e1bb47844e2cbdb83d8e76b3bb021
thur17 <arthurthur17@gmail.com>
2022-05-12 18:36:55 -0300

    Projeto criado

---
+++ Projeto_Termo.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root=Tk()
+
+root.configure(background="#F4A460")
+
+class Application():
+    def __init__(self):
+        self.root = root   
+        self.tela()
+        root.mainloop()
+    def tela(self):
+        self.root.title("Preenchimento de termo de Ativos")
+        self.root.configure(background="#F4A460")
+        self.root.geometry("1000x620")
+        self.root.wm_resizable(True,True)
+
+Application()
