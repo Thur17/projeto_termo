86fb2e009a9e837a1feb4374a5d5a4764f1df817
thur17 <arthurthur17@gmail.com>
2022-05-19 19:10:19 -0300

    Mater

--- Projeto_Termo.py
+++ Projeto_Termo.py
@@ -1,19 +1,31 @@
+from cProfile import label
+from email.mime import image
 from tkinter import *
 
 root=Tk()
 
 root.configure(background="#F4A460")
 
+
 class Application():
     def __init__(self):
         self.root = root   
         self.tela()
+        self.frames_da_tela()
         root.mainloop()
     def tela(self):
         self.root.title("Preenchimento de termo de Ativos")
         self.root.configure(background="#F4A460")
-        self.root.geometry("1000x620")
+        self.root.geometry("1000x800")
         self.root.wm_resizable(True,True)
+        self.root.maxsize(width=900, height=700)
+        self.root.minsize(width=400, height=300)
+    def frames_da_tela(self):
+        self.frame_1=Frame(self.root, bd=4, bg="#F4A460", highlightthickness=3 ) 
+        self.frame_1.place(relx=0.02,rely=0.02,relwidth=0.96,relheight=0.46)
+        self.frame_2=Frame(self.root, bd=4, bg="#F4A460", highlightthickness=3 ) 
+        self.frame_2.place(relx=0.02,rely=0.5,relwidth=0.96,relheight=0.46)
+
 
 
 
